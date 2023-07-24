1066b9f670e7c37e4b2bdc869a15d6eb43aed66c
MatthewKokin <135328101+MatthewKokin@users.noreply.github.com>
2023-07-24 21:40:51 +0200

    working on picking buttons

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Die from './Die/Die'
 import {nanoid} from 'nanoid'
+import { useEffect } from 'react'
 
 function App() {
   const [diesArr, setDiesArr] = useState(dies())
@@ -10,21 +11,24 @@ function App() {
   function dies() {
     let arr = [];
     for (let i = 0; i < 10; i++) {
-      arr.push({num: Math.floor(Math.random() * 6) + 1, isPicked: false});
+      arr.push({num: Math.floor(Math.random() * 6) + 1, isPicked: false, id: nanoid()});
   }
   return arr;
 }
 
-  function pick(){
-    console.log("HEy")
+  function pick(id, value){
+    setDiesArr(oldVal => oldVal.map(die => die.id === id? {...die, isPicked: !die.isPicked}: die))
+    console.log(id, value)
   }
+
+  useEffect(() => console.log(diesArr.filter(die => die.isPicked === true)), [diesArr])
 
   function roll(){
     setDiesArr(dies())
   }
 
   const DiesEl = diesArr.map(die => {
-    return <Die key={nanoid()} value={die.num} handleClick={pick}/>
+    return <Die key={die.id} id={die.id} value={die.num} handleClick={pick}/>
   })
 
   return (
--- src/Die/Die.jsx
+++ src/Die/Die.jsx
@@ -4,7 +4,7 @@ function Die(props) {
     return (
         <>
             <div className='die-face'>
-                <h2 className="die-num">{props.value}</h2>
+                <h2 className="die-num" onClick={()=> props.handleClick(props.id, props.value)}>{props.value}</h2>
             </div>
         </>
     )
