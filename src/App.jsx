17194d07651433f79e45ff76afec17d1f8b4e3b9
MatthewKokin <135328101+MatthewKokin@users.noreply.github.com>
2023-07-25 10:57:04 +0200

    Works now. code

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,82 @@
-import { useState } from 'react'
 import './App.css'
 import Die from './Die/Die'
-import {nanoid} from 'nanoid'
+import { nanoid } from 'nanoid'
 import { useEffect } from 'react'
+import { useState } from 'react'
 
 function App() {
-  const [diesArr, setDiesArr] = useState(dies())
-  const [pickedArr, setPickedArr] = useState([])
+  const [diesArr, setDiesArr] = useState(getTenDies())
+  const [tenzies, setTenzies] = useState(false)
 
-  function dies() {
+  useEffect(() => {
+    const pickedDies = diesArr.filter(die => die.isPicked)
+    if (pickedDies.length === 10) {
+      for (let i = 0; i < 10; i++) {
+        if (diesArr[0].num === diesArr[i].num) {
+          setTenzies(true)
+        } else {
+          break
+        }
+      }
+    }
+  }, [diesArr])
+
+  /*
+  Scrimba
+  const allPicked = diesArr.every(die => die.Picked) //either true or false
+    const firstValue = diesArr[0].value
+    const allSameValue = diesArr.every(die => die.value === firstValue)
+    if (allPicked && allSameValue) {
+      setTenzies(true)
+      console.log("You won!")
+  */
+
+  function getTenDies() {
     let arr = [];
     for (let i = 0; i < 10; i++) {
-      arr.push({num: Math.floor(Math.random() * 6) + 1, isPicked: false, id: nanoid()});
-  }
-  return arr;
-}
-
-function pick(id){
-  setDiesArr(oldVal => {
-    const updatedDies = oldVal.map(die => die.id === id ? {...die, isPicked: !die.isPicked} : die);
-    const pickedDie = updatedDies.find(die => die.id === id);
-    if (pickedDie.isPicked){
-      setPickedArr(prevArr => [...prevArr, pickedDie])
-    } else {
-      setPickedArr(prevArr => prevArr.filter(die => die.id !== id))
+      arr.push(generateNewDie());
     }
-    return updatedDies;
-  })
-}
+    return arr;
+  }
 
+  function generateNewDie() {
+    return {
+      num: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid()
+    }
+  }
 
-  //---------------
-  useEffect(() => console.log(diesArr.filter(die => die.isPicked === true)), [diesArr])
-  useEffect(() => {
-    console.log("Picked array: ", pickedArr);
-  }, [pickedArr]);
-  
-  //---------------
+  function pick(id) {
+    setDiesArr(oldVal => {
+      const updatedDies = oldVal.map(die => die.id === id ? { ...die, isPicked: !die.isPicked } : die)
+      return updatedDies;
+    })
+  }
 
-  function roll(){
-    setDiesArr(dies())
+  function roll() {
+    setDiesArr(oldVal => {
+      return oldVal.map(die => die.isPicked ? die : generateNewDie())
+    })
   }
 
   const DiesEl = diesArr.map(die => {
-    return <Die key={die.id} id={die.id} value={die.num} handleClick={pick} isPicked={die.isPicked}/>
+    return <Die key={die.id} id={die.id} value={die.num} handleClick={pick} isPicked={die.isPicked} />
   })
 
   return (
-    <div className='container'>
-      <h1>Tenzies</h1>
-      <p>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
-      <div className='dice-container'>
-        {DiesEl}
-      </div>
-      <button onClick={roll}>Roll</button>
-    </div>
+    <>
+      {tenzies ? <p>You Won!!!</p> :
+        <div className='container'>
+          <h1>Tenzies</h1>
+          <p>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+          <div className='dice-container'>
+            {DiesEl}
+          </div>
+          <button onClick={roll}>Roll</button>
+        </div>
+      }
+    </>
   )
 }
 
