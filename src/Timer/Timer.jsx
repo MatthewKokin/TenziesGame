57aa5af0a547d2f5f10fcb874310aabe3cfc466e
MatthewKokin <135328101+MatthewKokin@users.noreply.github.com>
2023-07-29 21:08:16 +0200

    Working on css

--- src/Timer/Timer.jsx
+++ src/Timer/Timer.jsx
@@ -1,8 +1,14 @@
 import { useState, useEffect } from 'react'; // Don't forget to import React
 import './Timer.css'
+import PropTypes from 'prop-types';
 
-function Timer({isRunning}) {
-  const [passedTime, setPassedTime] = useState(0);
+function Timer({ isRunning }) {
+  const [passedTime, setPassedTime] = useState(0)
+  //Prop validation
+  Timer.propTypes = {
+    isRunning: PropTypes.bool,
+    handleClick: PropTypes.func.isRequired,
+  }  
 
   useEffect(() => {
     let interval;
@@ -19,10 +25,24 @@ function Timer({isRunning}) {
     return () => clearInterval(interval);
   }, [isRunning, passedTime]);
 
-  const time = (passedTime / 1000).toFixed(3);
+  const time = (passedTime / 1000).toFixed(3)
 
   return (
     <div className='container'>
+      <div className="cloader">
+        <div className="clface">
+          <div className="clsface">
+            <div id="h2" className="hand"></div>
+          </div>
+          <div className="top"></div>
+          <div className="bottom"></div>
+          <div className="left"></div>
+          <div className="right"></div>
+          <div id="sub" className="pin"></div>
+          <div id="h1" className="hand"></div>
+          <div id="main" className="pin"></div>
+        </div>
+      </div>
       <p>{time}</p>
     </div>
   )
