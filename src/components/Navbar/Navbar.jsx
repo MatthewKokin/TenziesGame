3409a4dbd25c05b6c5b5031b140e69d3bfb15c89
MatthewKokin <135328101+MatthewKokin@users.noreply.github.com>
2023-07-30 23:07:24 +0200

    working

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,18 +2,19 @@ import './Navbar.css'
 import { Link } from "react-router-dom"
 
 function Navbar() {
+    const styles = {backgroundColor: "#D3CDFE"}
     return (
         <div className='container-fluid p-0'>
-            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <nav className="navbar navbar-expand-sm" style={styles}>
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">Navbar</a>
+                    <Link to="/" className='navbar-brand'>Tenzies</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <Link to="/" className='nav-link'>Home</Link>
+                                <Link to="/multiplayer" className='nav-link'>Multiplayer</Link>
                             </li>
                             <li className="nav-item">
                                 <Link to="/leaderboard" className='nav-link'>Leaderboard</Link>
