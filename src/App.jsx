ac976902447dcbacbf308e23b9fc2f1a37633410
MatthewKokin <135328101+MatthewKokin@users.noreply.github.com>
2023-07-30 19:20:00 +0200

    new navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,20 @@
 import './App.css'
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import { BrowserRouter, Routes, Route} from "react-router-dom"
 import Leaderboard from './components/Leaderboard/Leaderboard';
 import Computer from './components/Computer/Computer';
 import Rules from './components/Rules/Rules';
 import Home from './components/Home/Home';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <BrowserRouter>
-    <div className='container-fluid p-0'>
-      <nav className='navbar nav-custom'>
-        <h2 className='nav-link badge bg-primary logo'>Tenzies</h2>
-        <Link to="/" className='nav-link'>Home</Link>
-        <Link to="/leaderboard" className='nav-link'>Leaderboard</Link>
-        <Link to="/computer" className='nav-link'>Computer</Link>
-        <Link to="/ruler" className='nav-link'>Rules</Link>
-      </nav>
-    </div>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/leaderboard" element={< Leaderboard/>} />
-        <Route path="/computer" element={< Computer/>} />
-        <Route path="/ruler" element={< Rules/>} />
+        <Route path="/leaderboard" element={< Leaderboard />} />
+        <Route path="/computer" element={< Computer />} />
+        <Route path="/ruler" element={< Rules />} />
       </Routes>
     </BrowserRouter>
   )
